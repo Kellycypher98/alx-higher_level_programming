a74b22071b41703f1590e1d2598414e7e56bb2a9
kellycypher98 <kelvinboateng94@outlook.com>
2023-04-04 23:08:04 -0700

    initial commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+This module adds two integers.
+"""
+
+def add_integer(a, b=98):
+    """
+    Function that adds two integers.
+
+    Args:
+        a: First number to be added.
+        b: Second number to be added.
+
+    Returns:
+        The sum of a and b.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
+
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,7 @@
+Why is Python programming great?
+What is an interactive test?
+Why are tests crucial?
+How can you write Docstrings to create tests?
+How can you write documentation for each module and function?
+What are the fundamental option flags to create tests?
+How to identify edge cases.
